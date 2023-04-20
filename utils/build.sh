71acf01e57d38058cb202fa14926de8ebad26536
Masanori HIRANO <masa.hirano.1996@gmail.com>
2023-04-20 17:03:37 +0900

    update build script

--- utils/build.sh
+++ utils/build.sh
@@ -1,3 +1,5 @@
 cd $(dirname $0)/..
 jq -s add `find datasets/*/data/ -name "*.json" | sort` > data.json
 jq -s add `find datasets/*/data/ datasets-cc-by-sa/*/data/ -name "*.json" | sort` > data-cc-by-sa.json
+echo "data.json:" `jq 'length' data.json`
+echo "data-cc-by-sa.json:" `jq 'length' data-cc-by-sa.json`

711f658516a482d36321a5718b455646fe5ddff7
Masanori HIRANO <masanorihirano@users.noreply.github.com>
2023-04-28 17:12:18 +0900

    added coursera parallel corpus (#27)

---
+++ datasets/CourseraParallelCorpus/generate.py
@@ -0,0 +1,47 @@
+import os.path
+import pandas as pd
+from zipfile import ZipFile
+import json
+
+file_dir = os.path.dirname(__file__)
+
+zip_file = ZipFile(os.path.join(file_dir, "master.zip"), mode="r")
+dfs = []
+for type_str in ["train", "dev", "test"]:
+    df_ja = pd.read_csv(zip_file.open(f"CourseraParallelCorpusMining-master/data/Coursera_En-Ja/{type_str}.ja.txt"), sep="\t", header=None, quoting=3).reset_index(drop=True)
+    df_ja = df_ja.rename(columns={0: "ja"})
+    df_en = pd.read_csv(zip_file.open(f"CourseraParallelCorpusMining-master/data/Coursera_En-Ja/{type_str}.en.txt"), sep="\t", header=None, quoting=3).reset_index(drop=True)
+    df_en = df_en.rename(columns={0: "en"})
+    print(len(df_en), len(df_ja))
+    assert len(df_en) == len(df_ja), type_str
+    df_ja["en"] = df_en["en"]
+    df_ja["ja"] = df_ja["ja"].str.replace(" ", "")
+    df_ = df_ja
+    dfs.append(df_)
+df = pd.concat(dfs)
+df["instruction"] = "次の日本語を英語に翻訳してください。"
+df["index"] = "F" + df.index.astype(str)
+df["input"] = df["ja"]
+df["output"] = df["en"]
+data_list = df[["index", "instruction", "input", "output"]].to_dict(orient="records")
+forward_n = len(data_list) // 1000 + 1
+for i in range(forward_n):
+    file_name = os.path.join(file_dir, "data", f"{i:0>6}.json")
+    json.dump(
+        data_list[i * 1000: min((i+1)*1000, len(data_list))],
+        open(file_name, mode="w", encoding="utf-8"),
+        indent=2, ensure_ascii=False
+    )
+df["instruction"] = "次の英語を日本語に翻訳してください。"
+df["index"] = "F" + df.index.astype(str)
+df["input"] = df["en"]
+df["output"] = df["ja"]
+data_list = df[["index", "instruction", "input", "output"]].to_dict(orient="records")
+reverse_n = len(data_list) // 1000 + 1
+for i in range(reverse_n):
+    file_name = os.path.join(file_dir, "data", f"{i + forward_n:0>6}.json")
+    json.dump(
+        data_list[i * 1000: min((i+1)*1000, len(data_list))],
+        open(file_name, mode="w", encoding="utf-8"),
+        indent=2, ensure_ascii=False
+    )

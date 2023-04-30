98ef66f0e6a2d4ba951c2d2dd23ee56653942dda
Masanori HIRANO <masa.hirano.1996@gmail.com>
2023-04-30 22:39:00 +0900

    update

--- utils/build.sh
+++ utils/build.sh
@@ -1,5 +1,5 @@
 cd $(dirname $0)/..
 jq -s add `find datasets/*/data/ -name "*.json" | sort` | jq -n --stream 'fromstream(1|truncate_stream(inputs)) | {instruction:.instruction, input:.input, output:.output}' | jq -s 'flatten' > data.json
-jq -s add `find datasets/*/data/ datasets-cc-by-sa/*/data/ -name "*.json" | sort` | jq '.[] | {instruction:.instruction, input:.input, output:.output}' | jq -s 'flatten' > data-cc-by-sa.json
+jq -s add `find datasets/*/data/ datasets-cc-by-sa/*/data/ -name "*.json" | sort` | jq -n --stream 'fromstream(1|truncate_stream(inputs)) | {instruction:.instruction, input:.input, output:.output}' | jq -s 'flatten' > data-cc-by-sa.json
 echo "data.json:" `jq '. | length' data.json`
 echo "data-cc-by-sa.json:" `jq '. | length' data-cc-by-sa.json`

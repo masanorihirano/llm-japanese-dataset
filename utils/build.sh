7ae7f52984815d464fe716247c74de5065d7afdf
Masanori HIRANO <masa.hirano.1996@gmail.com>
2023-04-30 22:57:51 +0900

    update script

--- utils/build.sh
+++ utils/build.sh
@@ -1,5 +1,5 @@
 cd $(dirname $0)/..
-jq -s add `find datasets/*/data/ -name "*.json" | sort` | jq -n --stream 'fromstream(1|truncate_stream(inputs)) | {instruction:.instruction, input:.input, output:.output}' > data.jsonl
-jq -s add `find datasets/*/data/ datasets-cc-by-sa/*/data/ -name "*.json" | sort` | jq -n --stream 'fromstream(1|truncate_stream(inputs)) | {instruction:.instruction, input:.input, output:.output}' > data-cc-by-sa.jsonl
-echo "data.json:" `jq '. | length' data.json`
-echo "data-cc-by-sa.json:" `jq '. | length' data-cc-by-sa.json`
+jq -s add `find datasets/*/data/ -name "*.json" | sort` | jq -n -c --stream 'fromstream(1|truncate_stream(inputs)) | {instruction:.instruction, input:.input, output:.output}' > data.jsonl
+jq -s add `find datasets/*/data/ datasets-cc-by-sa/*/data/ -name "*.json" | sort` | jq -n -c --stream 'fromstream(1|truncate_stream(inputs)) | {instruction:.instruction, input:.input, output:.output}' > data-cc-by-sa.jsonl
+wc -l data.jsonl
+wc -l data-cc-by-sa.jsonl
